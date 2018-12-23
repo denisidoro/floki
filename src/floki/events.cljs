9a9e02fdb3c3624d78845194c6759fcd2d510c75
Denis Isidoro <denis_isidoro@live.com>
2018-12-23 17:06:26 -0200

    wip

--- src/floki/events.cljs
+++ src/floki/events.cljs
@@ -1,6 +1,6 @@
 (ns floki.events
   (:require [re-frame.core :as rf]
-            [floki.logic :as l]))
+            [floki.movement.events]))
 
 (def input
   {:a {:b 1 :c {:d {:e 53}} :john 44}
@@ -14,41 +14,3 @@
      :pos/x 0
      :pos/y 0
      :tree/path  [:a]}))
-
-(rf/reg-event-db
-  :movement/down
-  (fn [db _]
-    (if (l/vertical-allowed? db 1)
-      (-> db
-                                  (update :pos/y inc)
-                                  (l/update-list 0))
-      db)))
-
-(rf/reg-event-db
-  :movement/up
-  (fn [db _]
-    (if (l/vertical-allowed? db -1)
-      (-> db
-                                  (update :pos/y dec)
-                                  (l/update-list 0))
-      db)))
-
-(rf/reg-event-db
-  :movement/right
-  (fn [db _]
-    (if (l/horizontal-allowed? db 1)
-      (-> db
-                                         (assoc :pos/y 0)
-                                         (update :pos/x inc)
-                                         (l/update-list 1))
-      db)))
-
-(rf/reg-event-db
-  :movement/left
-  (fn [db _]
-    (if (l/horizontal-allowed? db -1)
-      (-> db
-                                          (assoc :pos/y 0)
-                                          (update :pos/x dec)
-                                          (l/update-list -1))
-      db)))
