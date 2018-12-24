2eceb6514f9d381244c50168de998fbf4b464e85
Denis Isidoro <denis_isidoro@live.com>
2018-12-24 11:00:40 -0200

    fix

--- src/floki/preview/view.cljs
+++ src/floki/preview/view.cljs
@@ -1,15 +1,29 @@
 (ns floki.preview.view
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [common.print :as print]))
 
 (defn preview
   []
   [:text
    {:left    0
     :top     0
-    :height  2
     :width   50
     :content (-> @(rf/subscribe [:preview/data])
-                 ;print/pprint-str
-                 str
-                 )
-    }])
+                 print/cstr)}])
+
+(defn without-first-and-last-chars
+  [s]
+  (subs s 1 (-> s count dec)))
+
+(defn path-box
+  []
+  [:text#debug {:bottom 0
+                :left   0
+                :width  "100%"
+                :style  {:border {:fg :yellow}}
+                :border {:type :line}
+                :label  "Path"}
+   [:text {:width   "40%"
+           :content (-> @(rf/subscribe [:tree/path])
+                         str
+                        without-first-and-last-chars)}]])
