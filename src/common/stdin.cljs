a44e00efcce4d9a71a82e811ddd75725f99c65a4
Denis Isidoro <denis_isidoro@live.com>
2018-12-31 10:21:44 -0200

    :nail_care:

--- src/common/stdin.cljs
+++ src/common/stdin.cljs
@@ -9,12 +9,12 @@
   (.setEncoding stdin "utf8")
   (.on stdin "data"
        (fn [data]
-         (print (str "DATA!!!!!!!" data))
+         ; (print (str "DATA!!!!!!!" data))
          (if (= (str data) "q")
            (process/exit))
          (swap! stdinput #(str % data))))
   (.on stdin "end"
        (fn []
-         (print "END EVENT!!!!")
+         ; (print "END EVENT!!!!")
          (swap! stdinput #(->> % count dec (subs % 0)))
          (callback @stdinput))))
